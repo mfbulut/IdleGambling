68e597637c6095d01b5b08ff4c5334e6522d88c0
Furkan <51883559+mfbulut@users.noreply.github.com>
2025-05-20 17:21:00 +0000

    Code cleanup + Move assets folder

--- build_wasm.bat
+++ build_wasm.bat
@@ -1,3 +1,3 @@
 call emsdk_env.bat
 
-emcc -o build/web/index.html src/main.c -Os -Wall ./raylib/wasm/lib/libraylib.a -I. -I./raylib/wasm/include -L. -L./raylib/wasm/lib/ -s USE_GLFW=3 -s TOTAL_MEMORY=67108864 -s SINGLE_FILE=1 --embed-file build/assets --shell-file ./raylib/wasm/minshell.html -DPLATFORM_WEB -s EXPORTED_RUNTIME_METHODS=ccall,HEAPF32
+emcc -o build/web/index.html src/main.c -Os -Wall ./raylib/wasm/lib/libraylib.a -I. -I./raylib/wasm/include -L. -L./raylib/wasm/lib/ -s USE_GLFW=3 -s TOTAL_MEMORY=67108864 -s SINGLE_FILE=1 --embed-file assets --shell-file ./raylib/wasm/minshell.html -DPLATFORM_WEB -s EXPORTED_RUNTIME_METHODS=ccall,HEAPF32
